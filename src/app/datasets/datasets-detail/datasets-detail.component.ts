1b3a4874774f8b63ac35319ede1a29351319b31d
Katrin Shechtman <katrin.shechtman@thomsonreuters.com>
2018-03-26 13:08:33 -0400

    adding graph and tree map to dataset details page

--- src/app/datasets/datasets-detail/datasets-detail.component.ts
+++ src/app/datasets/datasets-detail/datasets-detail.component.ts
@@ -22,8 +22,33 @@ export class DatasetsDetailComponent implements OnInit {
   single = [{name: '', value: 0}];
 
   colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    domain: ['#E37222', '#07889B', '#66B9BF', '#EEAA7B', '#FCEFEF']
   };
+
+  graphView: any[] = [700, 400];
+
+  graph = {
+      nodes: [
+        {value: "Myriel", group: 1},
+        {value: "Napoleon", group: 1}
+      ],
+      links: [
+         {source: "Napoleon", target: "Myriel", value: 1}
+      ]
+  };
+
+  relationsTreeMap = [
+    {
+      "name": "Germany",
+      "value": 40632
+    },
+    {
+      "name": "United States",
+      "value": 49737
+    }
+  ];
+
+  allDatasets: Dataset[];
 
   constructor(private route: ActivatedRoute, private datasetService: DatasetsService, private router: Router, private userService: UserService) { }
 
@@ -41,6 +66,10 @@ export class DatasetsDetailComponent implements OnInit {
           this.dataset = ds;
           this.datasetService.getAllDatasets()
             .subscribe( (datasets: Dataset[]) => {
+                this.allDatasets = datasets;
+                /*
+                ** chart
+                */
                 var counter = [];
                 this.dataset.categories.map((ct) => {counter[ct] = 0});
                 datasets.map((next) => {
@@ -50,6 +79,35 @@ export class DatasetsDetailComponent implements OnInit {
                   });
                 });
                 this.single = Object.entries(counter).map(([ct, count]) => ({name: ct, value: parseInt(count)}));
+                /*
+                ** graph
+                */
+                this.graph.nodes = [];
+                this.graph.links = [];
+                this.graph.nodes.push({value: this.dataset.name, group: 1});
+                datasets.map((next) => {
+                  this.graph.nodes.push({value: next.name, group: 1});
+                });
+                datasets.map((next) => {
+                  next.categories.map(ct => {
+                    if(this.dataset.categories.find(current => current == ct))
+                      this.graph.links.push({source: this.dataset.name, target: next.name, value: 1 });
+                  });
+                });
+                /*
+                ** Treemap
+                */
+                var counterTreeMap = [];
+                datasets.map((next) => {
+                  counterTreeMap[next.name] = 0;
+                  next.categories.map(ct => {
+                    if(this.dataset.categories.find(current => current == ct))
+                      counterTreeMap[next.name]++;
+                  });
+                });
+                this.relationsTreeMap = Object.entries(counterTreeMap)
+                .filter(([name, count]) => (name != this.dataset.name && count > 0))
+                .map(([name, count]) => ({name: name, value: parseInt(count)}));
             });
         }
       });
@@ -66,6 +124,16 @@ export class DatasetsDetailComponent implements OnInit {
     onChartSelect(event){
       if(this.userService.isConsumer())
         this.router.navigateByUrl(`/datasets?search=${event.name}`);
+    }
+
+    onGraphSelect(event){
+      var id = this.allDatasets.find(x => x.name == event.name)._id;
+      this.router.navigateByUrl(`/datasets/${id}`);
+    }
+
+    onSelectTreeMap(event){
+      var id = this.allDatasets.find(x => x.name == event.name)._id;
+      this.router.navigateByUrl(`/datasets/${id}`);
     }
 
     xAxisTickFormatting(value) {
