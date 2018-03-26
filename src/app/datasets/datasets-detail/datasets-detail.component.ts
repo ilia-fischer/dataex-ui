e7ee3e9e473e9a601842324419a5ee3cc4d3ccb3
Katrin Shechtman <katrin.shechtman@thomsonreuters.com>
2018-03-26 08:38:10 -0400

    chart works, maybe colors are off

--- src/app/datasets/datasets-detail/datasets-detail.component.ts
+++ src/app/datasets/datasets-detail/datasets-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 
 import { DatasetsService } from '../../shared/dataset.service';
 import { Dataset } from '../../shared/dataset.model';
+import { UserService } from '../../core/user.service';
 
 @Component({
   selector: 'trdx-datasets-detail.',
@@ -15,7 +16,16 @@ export class DatasetsDetailComponent implements OnInit {
   dataset: Dataset = null;
   idNotFound: Boolean = false;
 
-  constructor(private route: ActivatedRoute, private datasetService: DatasetsService) { }
+  //chart
+  view: any[] = [700, 400];
+
+  single = [{name: '', value: 0}];
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor(private route: ActivatedRoute, private datasetService: DatasetsService, private router: Router, private userService: UserService) { }
 
   ngOnInit() {
     this.route.params
@@ -27,16 +37,42 @@ export class DatasetsDetailComponent implements OnInit {
         if(ds == null || typeof ds === 'undefined'){
           this.idNotFound = true;
         }
-        else this.dataset = ds;
+        else {
+          this.dataset = ds;
+          this.datasetService.getAllDatasets()
+            .subscribe( (datasets: Dataset[]) => {
+                var counter = [];
+                this.dataset.categories.map((ct) => {counter[ct] = 0});
+                datasets.map((next) => {
+                  next.categories.map(ct => {
+                    if(ct in counter)
+                      counter[ct]++;
+                  });
+                });
+                this.single = Object.entries(counter).map(([ct, count]) => ({name: ct, value: parseInt(count)}));
+            });
+        }
       });
-  }
+    }
 
-  purchaseDataset(){
+    purchaseDataset(){
     // const initialState = {
     //   dataset: this.dataset
     // };
     // this.bsModalRef = this.modalService.show(PurchaseDatasetModalComponent, {initialState});
 
-  }
+    }
+
+    onChartSelect(event){
+      if(this.userService.isConsumer())
+        this.router.navigateByUrl(`/datasets?search=${event.name}`);
+    }
+
+    xAxisTickFormatting(value) {
+      if(parseInt(value) == value)
+        return parseInt(value);
+      else
+        return '';
+    }
 
 }
